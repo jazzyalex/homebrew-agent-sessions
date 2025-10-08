3f24b9a05c60a1a1f4c605b5dfbc4c962aeffba8
Alexander Malakhov <jazzyalex@gmail.com>
2025-10-08 16:22:48 -0700

    agent-sessions 2.2 (cask): update url and sha256

--- Casks/agent-sessions.rb
+++ Casks/agent-sessions.rb
@@ -1,6 +1,6 @@
 cask "agent-sessions" do
-  version "2.1"
-  sha256 "b962994e402e7cdfe575e28c37989ff96a2aab10172635a246cf7c59b97cda26"
+  version "2.2"
+  sha256 "6388a1dd04606ee59155586131f2e7b195213a9a01298d97627690ea69b4cae2"
 
   url "https://github.com/jazzyalex/agent-sessions/releases/download/v#{version}/AgentSessions-#{version}.dmg",
       verified: "github.com/jazzyalex/agent-sessions/"
